65a77f6d0d978a23cd1250cf1d9aaf8dafb58db2
MOHAMAD FAKHRULLAH BIN IBRAHIM <115959825+fakhrulyoi@users.noreply.github.com>
2025-08-28 11:40:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PORTFOLIO/', // <-- Add this line if deploying to a subfolder!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
