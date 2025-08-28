34948e523751f379bb9430d353d0a30857b0a426
MOHAMAD FAKHRULLAH BIN IBRAHIM <115959825+fakhrulyoi@users.noreply.github.com>
2025-08-28 11:43:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PORTFOLIO/', // <-- Add this line if deploying to a subfolder!
+  base: '/PORTFOLIO/', // <-- use exactly this!
   plugins: [react()],
 })
